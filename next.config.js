cf1f4703925e189c16898a24c55a6bde75e15f40
fushougang <fushougang@blacklake.cn>
2023-04-09 19:43:11 +0800

    添加二级路径

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-    reactStrictMode: true,
+    basePath: '/nextjs-nft-marketplace',
     images: {
         loader: "akamai",
         path: "",
