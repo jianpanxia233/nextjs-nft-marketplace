c9fd025cf127dd9735c30eb78a7cc1b4047a26e8
fushougang <fushougang@blacklake.cn>
2023-04-09 21:01:59 +0800

    修改gh-pages命令

--- add_nojekyll_file.js
+++ add_nojekyll_file.js
@@ -9,10 +9,5 @@ fs.writeFile(path.join(__dirname,'out/.nojekyll'), 'recognize static source', (e
     }
 
     // 创建成功
-    console.log(`创建成功！`)
-    ghpages.publish('out', { add: true }, function(err, success) {
-        console.log('err', err);
-        console.log('success', success);
-    })
-    
+    console.log(`创建成功！`)    
 })
